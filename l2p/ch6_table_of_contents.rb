84072494d754fb40e9a4bf5ceb84e15c9114cb42
Jason King <jasonk@amcoonline.net>
2013-04-19 13:30:55 -0700

    Table of Contents problem

---
+++ l2p/ch6_table_of_contents.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+CHAPTERS = [
+  { name: "Getting Started", page: 1 },
+  { name: "Numbers", page: 9 },
+  { name: "Letters", page: 13 },
+]
+
+WIDTH = `tput cols`.chomp.to_i - 50
+
+puts "Table of Contnts".center(WIDTH)
+CHAPTERS.each_with_index do |e, i|
+  puts "Chapter #{i+1}:  #{e[:name]}".ljust(WIDTH/2) + ( "page".ljust(4) + e[:page].to_s.rjust(3) ).rjust(WIDTH/2)
+end
