ea8fbcad9cd0c6bbf4ccb1a830f7795fb5d0c4cd
Jason King <jasonk@amcoonline.net>
2013-04-19 12:51:37 -0700

    My age in seconds

--- l2p/ch2.rb
+++ l2p/ch2.rb
@@ -10,3 +10,11 @@ MINUTES_PER_HOUR = 60
 YEARS_PER_DECADE = 10
 
 puts "Minutes in a decade = #{MINUTES_PER_HOUR * HOURS_PER_YEAR * YEARS_PER_DECADE}"
+
+SECONDS_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR * 60
+BIRTH_DATETIME = "1971-06-06 03:45:00 +1000"
+
+require 'date'
+days_since_birth = DateTime.now - DateTime.parse( BIRTH_DATETIME)
+
+puts "My age in seconds is: #{(days_since_birth * SECONDS_PER_DAY).to_i}"
