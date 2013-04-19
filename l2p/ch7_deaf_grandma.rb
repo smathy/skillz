8fbda368088ef142fda3ce41074658291d0f70b2
Jason King <jasonk@amcoonline.net>
2013-04-19 14:44:26 -0700

    Three BYEs

--- l2p/ch7_deaf_grandma.rb
+++ l2p/ch7_deaf_grandma.rb
@@ -1,11 +1,18 @@
 #!/usr/bin/env ruby
 
+bye_count = 0
 while true
   question = gets.chomp
 
   if question == "BYE"
-    exit
-  elsif question.upcase == question
+    if ( bye_count += 1 ) >= 3
+      exit
+    end
+  else
+    bye_count = 0
+  end
+
+  if question.upcase == question
     puts "NO, NOT SINCE #{rand 1930..1950}!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
