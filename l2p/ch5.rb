27c656c94d9948890350dec538292cc88bfabd8d
Jason King <jasonk@amcoonline.net>
2013-04-19 13:10:17 -0700

    Full name greeting

---
+++ l2p/ch5.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+print "What's your first name? "
+first_name = gets.chomp
+
+print "What's your middle name? "
+middle_name = gets.chomp
+
+print "What's your last name? "
+last_name = gets.chomp
+
+puts "Well hello there #{first_name} #{middle_name} #{last_name}"
