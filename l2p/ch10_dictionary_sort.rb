1b8c1984baea0423a9fa82407411469c7c6aa87f
Jason King <jasonk@amcoonline.net>
2013-05-13 12:24:32 -0700

    Recursive dictionary sort

--- l2p/ch10_dictionary_sort.rb
+++ l2p/ch10_dictionary_sort.rb
@@ -4,10 +4,10 @@ puts "Enter some words..."
 arr = STDIN.read.split "\n"
 
 def normal_sort unsorted
-  sorted = [ unsorted.pop ]
+  sorted = []
   unsorted.each do |t|
     catch :next_unsorted do
-      0.upto(sorted.length-1) do |i|
+      sorted.each_index do |i|
         if sorted[i] >= t
           sorted[i,0] = t
           throw :next_unsorted
@@ -19,4 +19,30 @@ def normal_sort unsorted
   sorted
 end
 
-puts "And the sorted words now: " + normal_sort(arr).join(", ")
+puts "Sorted non-recursively:"
+puts "\t" + normal_sort(arr).join(", ")
+
+def recursive_sort unsorted_array
+  _recursive_sort unsorted_array, []
+end
+
+def _recursive_sort unsorted, sorted
+  if needle = unsorted.pop
+    catch :next_unsorted do
+      sorted.each_with_index do |e, i|
+        if e >= needle
+          sorted[i,0] = needle
+          throw :next_unsorted
+        end
+      end
+      sorted.push needle
+    end
+    _recursive_sort unsorted, sorted
+  else
+    return sorted
+  end
+end
+
+puts
+puts "Sorted recursively..."
+puts "\t" + recursive_sort(arr).join(", ")
